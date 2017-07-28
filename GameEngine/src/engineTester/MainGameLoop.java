ff9e46990f76f6f46abe0a8b1cd29fd2335cf66f
buzka16 <buzka16@ya.ru>
2017-07-28 13:08:16 +0300

    Add files via upload

--- GameEngine/src/engineTester/MainGameLoop.java
+++ GameEngine/src/engineTester/MainGameLoop.java
@@ -1,34 +1,53 @@
 package engineTester;
 
+import models.TexturedModel;
 import org.lwjgl.opengl.Display;
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
-import renderEngine.RawModel;
+import models.RawModel;
 import renderEngine.Renderer;
+import shaders.StaticShader;
+import textures.ModelTexture;
 
 public class MainGameLoop {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         DisplayManager.createDisplay();
-        Loader loader=new Loader();
-        Renderer renderer=new Renderer();
+        Loader loader = new Loader();
+        Renderer renderer = new Renderer();
+        StaticShader shader = new StaticShader();
 
-        float[] vertices = { -0.5f, 0.5f, 0f,
+        float[] vertices = {-0.5f, 0.5f, 0f,
                 -0.5f, -0.5f, 0f,
                 0.5f, -0.5f, 0f,
-                0.5f, -0.5f, 0f,
-                0.5f, 0.5f, 0f,
-                -0.5f, 0.5f, 0f };
+                0.5f, 0.5f, 0f};
 
-        RawModel model = loader.loadToVAO(vertices);
+        int[] indices = {
+                0, 1, 3,//top left tri
+                3, 1, 2// bottom right tri
+        };
 
-        while(!Display.isCloseRequested()){
+        float[] textureCoords = {
+                0, 0,//V0
+                0, 1,//V1
+                1, 1,//V3
+                1, 0,//V3
+        };
+
+        RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
+        ModelTexture texture = new ModelTexture(loader.loadTexture("image.png"));
+        TexturedModel texturedModel = new TexturedModel(model, texture);
+        while (!Display.isCloseRequested()) {
             renderer.prepare();
+            shader.start();
             //game logic
             // render
-            renderer.render(model);
+            renderer.render(texturedModel);
+            shader.stop();
             DisplayManager.updateDisplay();
         }
+
+        shader.cleanUp();
         loader.cleanUp();
         DisplayManager.closeDisplay();
     }
